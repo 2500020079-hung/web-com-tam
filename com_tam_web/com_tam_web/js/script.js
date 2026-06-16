05713230a3304a529b26251d50317c715ffa9295
2500020079-hung <2500020079@nttu.edu.vn>
2026-06-16 22:13:19 +0700

    Add files via upload

---
+++ com_tam_web/com_tam_web/js/script.js
@@ -0,0 +1,7 @@
+
+function datMon() {
+    alert("Bạn đã gửi đơn đặt món thành công! Quán sẽ liên hệ xác nhận.");
+}
+function lienHe() {
+    alert("Cảm ơn bạn đã góp ý cho quán Cơm Tấm Sườn Bì Chả!");
+}
